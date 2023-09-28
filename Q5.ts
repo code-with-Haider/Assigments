fcc8ab1ee63c8d98c9611a47b43f87cb995816fc
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-09-28 18:48:09 +0500

    Exercise 5

---
+++ Q5.ts
@@ -0,0 +1,6 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+//"Success is not final, failure is not fatal: It is the courage to continue that counts." - Winston Churchill
+//console.log("Winston Churchill once said : Success is not final, failure is not fatal: It is the courage to continue that counts." )
+let famous_Person ="Winston Churchill ";
+let message = famous_Person  + 'Once said :Success is not final, failure is not fatal: It is the courage to continue that counts.'
+console.log(message);
