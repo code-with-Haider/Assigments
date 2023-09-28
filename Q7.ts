c8f98ebd2728ba8d7d5b34db724340d45e9be3f4
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-09-28 19:06:18 +0500

    exercise 7

---
+++ Q7.ts
@@ -0,0 +1,20 @@
+
+ //Addition
+const additionResult = 4 + 4;
+console.log("Addition Result: " + additionResult);
+
+ //Subtraction
+const subtractionResult = 16 - 8;
+console.log("Subtraction Result: " + subtractionResult);
+
+ //Multiplication
+const multiplicationResult = 2 * 4;
+console.log("Multiplication Result: " + multiplicationResult);
+
+//Division
+const divisionResult = 64 / 8;
+console.log("Division Result: " + divisionResult);
+
+
+
+
