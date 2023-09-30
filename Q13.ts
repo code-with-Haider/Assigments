a7d100f9b7fa60923949ea99c93b56751dd4ccde
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 02:03:53 +0500

    exercise 13

---
+++ Q13.ts
@@ -0,0 +1,7 @@
+
+const favoriteTransportation: string[] = ["Honda motorcycle", "Tesla car", "Vespa scooter"];
+
+
+favoriteTransportation.forEach((transportation) => {
+  console.log(`I would like to own a ${transportation}.`);
+});
