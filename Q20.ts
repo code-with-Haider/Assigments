2d2772103a64080556140d3ea4801eead12b8b7e
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 02:34:16 +0500

    exercise 20

---
+++ Q20.ts
@@ -0,0 +1,12 @@
+
+
+const countries: string[] = ["USA", "Pakistan", "Australia", "Japan", "Chaina"]
+
+console.log("List of Countries:")
+
+for (let country of countries)
+{
+ console.log(country)
+}
+
+export{};
