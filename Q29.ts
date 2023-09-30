c2865a55c7e6aa3670b4e13c114f13c856b7f3a2
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 03:03:15 +0500

    exercise29

---
+++ Q29.ts
@@ -0,0 +1,27 @@
+
+// Define the array of fruits
+const favorite_fruits: string[] = ["apple", "banana", "orange"];
+/// syntex: arrayName.includes("element")
+// Check for specific fruits using independent if statements
+
+if (favorite_fruits.includes("apple")) {
+    console.log("I like apples!");
+}
+
+if (favorite_fruits.includes("banana")) {
+    console.log("I enjoy bananas!");
+}
+
+if (favorite_fruits.includes("orange")) {
+    console.log("Oranges are delicious!");
+}
+
+if (favorite_fruits.includes("grape")) {
+    console.log("Grapes are tasty!");
+}
+
+if (favorite_fruits.includes("mango")) {
+    console.log("Mangoes are one of my favorites!");
+} 
+
+export{};
