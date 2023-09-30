ef2d6714bd85c321fc419281b53f2d2b4a86792c
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 02:54:45 +0500

    exercise 26

---
+++ Q26.ts
@@ -0,0 +1,24 @@
+
+console.log(`\n1st version of code`)
+
+let alien_color: string = 'green';
+
+if (alien_color === 'green')
+{
+    console.log('Congratulations! You just earned 5 points for shooting the alien.');
+} else {
+    console.log('Congratulations! You just earned 10 points for shooting the alien.');
+}
+
+// version 2 ( runs the else block.) 
+console.log(`\n2nd version of code`)
+alien_color = 'blue';
+
+if (alien_color === 'green')
+{
+    console.log('Congratulations! You just earned 5 points for shooting the alien.');
+} else {
+    console.log('Congratulations! You just earned 10 points for shooting the alien.');
+}
+
+export{};
