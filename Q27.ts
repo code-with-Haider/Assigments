a5ea226ab0fdc16938919dddf324b3105a500f2e
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 02:56:35 +0500

    exercise 27

---
+++ Q27.ts
@@ -0,0 +1,52 @@
+
+console.log(`1st version where align color is green`)
+
+let alien_color: string = 'green';
+
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+} else if (alien_color === 'yellow') {
+    console.log('Congratulations! You just earned 10 points.');
+} else if (alien_color === 'red') {
+    console.log('Congratulations! You just earned 15 points.');
+} else {
+    console.log('Unknown alien color.');
+}
+
+
+//version 2 - where align color is yellow
+
+console.log(`2nd version where align color is yellow`)
+
+alien_color = 'yellow';
+
+if (alien_color === 'green')
+{
+    console.log('Congratulations! You just earned 5 points.');
+}
+else if (alien_color === 'yellow')
+{
+    console.log('Congratulations! You just earned 10 points.');
+}
+else if (alien_color === 'red')
+{
+    console.log('Congratulations! You just earned 15 points.');
+}
+else {
+    console.log('Unknown alien color.');
+}
+// 3rd version where align color is red
+console.log(`3rd version where align color is red`)
+alien_color = 'red';
+
+if (alien_color === 'green') {
+    console.log('Congratulations! You just earned 5 points.');
+} else if (alien_color === 'yellow') {
+    console.log('Congratulations! You just earned 10 points.');
+} else if (alien_color === 'red') {
+    console.log('Congratulations! You just earned 15 points.');
+} else {
+    console.log('Unknown alien color.');
+}
+
+export{};
