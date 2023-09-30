0e2b6b8e650fa50a10e29071ff163025233be676
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 02:45:44 +0500

    exercise23

---
+++ Q23.ts
@@ -0,0 +1,36 @@
+
+
+let car = 'Audi';
+
+console.log("Is car == 'Audi'? I predict True.");
+console.log(car == 'subaru'); // True
+
+console.log("Is car == 'honda'? I predict False.");
+console.log(car == 'honda'); // False
+
+console.log("Is car === 'Audi'? I predict True.");
+console.log(car === 'Audi'); // True
+
+console.log("Is car === 'Audi'? I predict False.");
+console.log(car === 'Audi'); // False
+
+console.log("Is car !== 'toyota'? I predict True.");
+console.log(car !== 'toyota'); // True
+
+console.log("Is car !== 'subaru'? I predict False.");
+console.log(car !== 'subaru'); // False
+
+console.log("Is car != 'mazda'? I predict True.");
+console.log(car != 'mazda'); // True
+
+console.log("Is car != 'Audi'? I predict False.");
+console.log(car != 'Audi'); // False
+
+let age = 25;
+console.log("Is age > 18? I predict True.");
+console.log(age > 18); // True
+
+console.log("Is age < 21? I predict False.");
+console.log(age < 21); // False
+
+export{};
