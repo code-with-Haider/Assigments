196cc485a232c0fb224e20d877576115a198c154
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 02:53:04 +0500

    exercise 25

---
+++ Q25.ts
@@ -0,0 +1,16 @@
+
+
+let alien_color = 'grey'
+
+if (alien_color == 'grey') {
+    console.log("Congratulations! You just earned 5 points.")
+}
+
+
+alien_color = 'blue'
+
+if (alien_color == 'grey') {
+    console.log("Congratulations! You just earned 5 points.")
+}
+
+export{};
