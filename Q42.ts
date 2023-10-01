1c743b2801bdc99ec6815e92190f15ec8361f754
Haidar Ali <146254761+code-with-Haider@users.noreply.github.com>
2023-10-01 17:20:28 +0500

    exercise42

---
+++ Q42.ts
@@ -0,0 +1,21 @@
+
+// Array of magician's names
+const magicianNames: string[] = ["Harry Porter", "David Copperfield", "Hermione Granger", "Professor Albus Dumbledore", "Ron Weasley"];
+
+function show_magicians(magicians: string[]): void {
+    for (const magician of magicians) {
+        console.log(magician);
+    }
+}
+
+function make_great(magicians: string[]): string[] {
+    const greatMagicians: string[] = magicians.map(magician => `the Great ${magician}`);
+    return greatMagicians;
+}
+
+
+// Modify the array to add "the Great" to each magician's name
+const greatMagicianNames: string[] = make_great(magicianNames);
+// Call the function to show the great magician's names
+console.log("\nGreat Magicians:");
+show_magicians(greatMagicianNames);
